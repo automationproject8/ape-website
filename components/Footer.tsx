4c934be833be8b5a3e2adf11aec76e087c76c463
automationproject8 <automation.project8@gmail.com>
2026-02-25 09:32:48 +0000

    logo2

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,7 +10,7 @@ export default function Footer() {
           <div>
             <div className="flex items-center gap-3 mb-4">
               <img
-                src="/logo1-ape.png"
+                src="/logo2-ape.png"
                 alt="Advanced Pro Engineering"
                 className="h-12 w-auto object-contain"
               />
